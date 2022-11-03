c4b0a5253929fbdfc4ccf7926e34f47c620b3544
Alexander <100308427+SsanchesS@users.noreply.github.com>
2022-11-03 18:43:04 +0300

    Update parsing.py

--- Python telegram_bot v2/parsing/parsing.py
+++ Python telegram_bot v2/parsing/parsing.py
@@ -94,6 +94,10 @@ def fill_db():
          INSERT INTO stirlitz (stirlitz_joke) VALUES ('{joke}');''')
          db.commit() 
       print("stirlitz добавлены")
+      cur.executescript(f'''
+         INSERT INTO users_jokes (joke) VALUES ('Девушка, можно вас на минутку?
+- Нет, у меня почасовой тариф.');''')
+      db.commit() 
       print("БД заполнена")
       cur.close()
       db.close()
